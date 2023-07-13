527f88723212f57dcc25c6bd71b0274771f92057
Parth Bisht <bishtparth795@gmail.com>
2023-07-13 18:51:14 +0530

    creating user in mongoDB

--- mongo-golang/controllers/user.go
+++ mongo-golang/controllers/user.go
@@ -30,7 +30,7 @@ func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httpr
 
 	u := models.User{}
 
-	if err := uc.Session.DB("mongo-golang").C("users").FindId(oid).One(&u); err != nil {
+	if err := uc.session.DB("mongo-golang").C("users").FindId(oid).One(&u); err != nil {
 		w.WriteHeader(404)
 		return
 	}
@@ -42,5 +42,24 @@ func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httpr
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+
+func (uc UserController) CreateUser(w http.ResponseWriter,r *http.Request,_ httprouter.Params){
+	u := models.User{}
+
+	json.NewDecoder(r.Body).Decode(&u)
+
+	u.Id = bson.NewObjectId()
+
+	uc.session.DB("mongo-golang").C("users").Insert(u)
+
+	uj,err := json.Marshal(u)
+	if err != nil{
+		fmt.Println(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
 	fmt.Fprintf(w, "%s\n", uj)
 }
